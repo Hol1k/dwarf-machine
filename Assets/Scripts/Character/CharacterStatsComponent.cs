9a4b509172fb1d49ea25bc62010953991770c51a
Hol1k <danilugryumov96@gmail.com>
2025-10-03 17:41:54 +0500

    added IDamageable and CharacterStatsComponent with it

---
+++ Assets/Scripts/Character/CharacterStatsComponent.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+using UnityEngine;
+
+namespace Character
+{
+    public class CharacterStatsComponent : MonoBehaviour, IDamageable
+    {
+        private float _maxHealth;
+        public float MaxHealth
+        {
+            get => _maxHealth;
+            set
+            {
+                _maxHealth = Mathf.Max(value, 0);
+                if (_health > value) _health = value;
+            }
+        }
+
+        private float _health;
+        public float Health
+        {
+            get => _health;
+            set => _health = Mathf.Clamp(value, 0, _maxHealth);
+        }
+        
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/IDamageable.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+    public interface IDamageable
+    {
+        public float MaxHealth { get; set; }
+        public float Health { get; set; }
+        
+        public void TakeDamage(float damage);
+    }
+}
