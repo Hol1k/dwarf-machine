60047cccc46f56d0ce8bc9385297537d68361e8c
Hol1k <danilugryumov96@gmail.com>
2025-10-17 17:49:57 +0500

    added a simple test enemy

---
+++ Assets/Scripts/Player/EnemyComponent.cs
@@ -0,0 +1,50 @@
+﻿using Entities;
+using UnityEngine;
+
+namespace Player
+{
+    public class EnemyComponent : MonoBehaviour, IDamageable
+    {
+        [SerializeField] EnemyStatsConfig enemyStatsConfig;
+        
+        private float _maxHealth;
+        public float MaxHealth
+        {
+            get => _maxHealth;
+            set
+            {
+                _maxHealth = Mathf.Max(value, 0);
+                if (_health > value) _health = value;
+            }
+        }
+
+        private float _health;
+        public float Health
+        {
+            get => _health;
+            set => _health = Mathf.Clamp(value, 0, _maxHealth);
+        }
+
+        private void Start()
+        {
+            InitializeStatsConfig();
+        }
+
+        private void InitializeStatsConfig()
+        {
+            if (enemyStatsConfig)
+            {
+                MaxHealth = enemyStatsConfig.maxHealth;
+                Health = enemyStatsConfig.currentHealth;
+            }
+            else
+                Debug.LogWarning("Start character stats are not set. Set to default values");
+        }
+
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+            Debug.Log($"{gameObject.name} damaged by {damage} damage\r\nCurrent health: {Health}");
+        }
+    }
+}
---
+++ Assets/Scripts/Player/EnemyStatsConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "NewEnemyStatsConfig", menuName = "Configs/EnemyStatsConfig", order = 0)]
+    public class EnemyStatsConfig : ScriptableObject
+    {
+        public float maxHealth = 100f;
+        public float currentHealth = 100f;
+    }
+}
