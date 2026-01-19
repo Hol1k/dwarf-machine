afd52df7b09e389b02d724276f0d46e6afdeaca0
Hol1k <danilugryumov96@gmail.com>
2026-01-19 09:36:01 +0500

    feat(enemy): implemented idle state for Melee Aborigine enemy

--- Assets/Scripts/Enemy/MeleeAborigineStates.cs
+++ Assets/Scripts/Enemy/MeleeAborigineStates.cs
@@ -1,10 +1,39 @@
-﻿namespace Enemy
+﻿using UnityEngine;
+
+namespace Enemy
 {
     public static class MeleeAborigineStates
     {
         public static EnemyFsmState GetIdleState()
         {
-            return new EnemyFsmState(null, null, null);
+            void Enter(EnemyAiContext aiContext, EnemyFsmContext fsmContext)
+            {
+                aiContext.ForgetLastSeePosition();
+                fsmContext.IdleTimer = Random.Range(1f, 3f);
+            }
+            
+            void Update(EnemyAiContext aiContext, EnemyFsmContext fsmContext)
+            {
+                if (aiContext.IsSeeTarget)
+                {
+                    fsmContext.RequestedState = EnemyFsmStateId.Combat;
+                }
+                else if (fsmContext.IdleTimer > 0)
+                {
+                    fsmContext.IdleTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    fsmContext.RequestedState = EnemyFsmStateId.Patrol;
+                }
+            }
+
+            void Exit(EnemyAiContext aiContext, EnemyFsmContext fsmContext)
+            {
+                fsmContext.IdleTimer = 0f;
+            }
+
+            return new EnemyFsmState(Enter, Update, Exit);
         }
 
         public static EnemyFsmState GetPatrolState()
