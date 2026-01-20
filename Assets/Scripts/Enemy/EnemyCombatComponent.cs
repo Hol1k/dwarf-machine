5b2d96dfb54e48ce39ba43638a748c66a53e56da
Hol1k <danilugryumov96@gmail.com>
2026-01-20 10:40:07 +0500

    refactor: make EnemyCombatComponent abstract and add SoldierCombatComponent

--- Assets/Scripts/Enemy/EnemyCombatComponent.cs
+++ Assets/Scripts/Enemy/EnemyCombatComponent.cs
@@ -4,11 +4,11 @@ using Zenject;
 
 namespace Enemy
 {
-    public class EnemyCombatComponent : MonoBehaviour
+    public abstract class EnemyCombatComponent : MonoBehaviour
     {
-        private EnemyAiContext _aiContext;
+        protected EnemyAiContext AiContext;
 
-        public bool CanAttackTarget => _aiContext.IsSeeTarget;
+        public virtual bool CanAttackTarget => false;
 
         public bool IsTargetEliminated
         {
@@ -20,61 +20,16 @@ namespace Enemy
                 return true;
             }
 
-            private set => _isTargetEliminated = value;
+            protected set => _isTargetEliminated = value;
         }
         private bool _isTargetEliminated;
-        
-        [SerializeField] private LayerMask hitObjectsMask;
-
-        [Space]
-        [SerializeField] [Tooltip("By Degrees")] [Min(0)] private float scutterValue;
-        [SerializeField] [Min(0.0000001f)] private float raycastWidth;
-        [SerializeField] [Min(0f)] private float maxShootDistance;
-
-        [Space]
-        [SerializeField] [Min(0f)] private float damage;
-        [SerializeField] [Min(0.0000001f)] [Tooltip("Hits per minute")] private float attackSpeed;
-
-        private float _lastAttackTime;
 
         [Inject]
-        private void Init(EnemyAiContext aiContext)
+        protected void Init(EnemyAiContext aiContext)
         {
-            _aiContext = aiContext;
+            AiContext = aiContext;
         }
 
-        public void AttackTarget(CharacterStatsComponent target)
-        {
-            if (Time.time - _lastAttackTime < 60f / attackSpeed)
-                return;
-
-            //Calculating aim
-            var normalizedShootDirection = (target.transform.position - transform.position).normalized;
-
-            var randomScutterValue = Random.insideUnitCircle * scutterValue;
-            
-            Quaternion shootRotationWithScutter = Quaternion.LookRotation(normalizedShootDirection);
-            shootRotationWithScutter *= Quaternion.Euler(
-                randomScutterValue.x,
-                randomScutterValue.y,
-                0);
-            Vector3 rotatedDirection = shootRotationWithScutter * Vector3.forward;
-            
-            //Shoot
-            if (Physics.SphereCast(transform.position, raycastWidth, rotatedDirection,
-                    out RaycastHit hitInfo, maxShootDistance, hitObjectsMask))
-            {
-                if (hitInfo.collider.TryGetComponent(out CharacterStatsComponent characterStats))
-                {
-                    characterStats.TakeDamage(damage);
-                    if (characterStats.IsDied)
-                    {
-                        IsTargetEliminated = true;
-                    }
-                }
-            }
-            
-            _lastAttackTime = Time.time;
-        }
+        public abstract void AttackTarget(CharacterStatsComponent target);
     }
 }
