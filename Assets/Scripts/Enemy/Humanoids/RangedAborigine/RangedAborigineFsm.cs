eeb3d3e46164864dbbc22c1e8b0de82004166bd6
Hol1k <danilugryumov96@gmail.com>
2026-01-31 09:17:23 +0500

    feat(enemy): create RangedAborigineFsm with its factory with no implemented states

---
+++ Assets/Scripts/Enemy/Humanoids/RangedAborigine/RangedAborigineFsm.cs
@@ -0,0 +1,22 @@
+﻿namespace Enemy.Humanoids.RangedAborigine
+{
+    public class RangedAborigineFsm : EnemyFsm
+    {
+        public RangedAborigineFsm(
+            EnemyFsmState idleState, 
+            EnemyFsmState patrolState, 
+            EnemyFsmState combatState, 
+            EnemyFsmState alertState, 
+            EnemyFsmState repositionState)
+        {
+            States.Add(EnemyFsmStateId.Idle,  idleState);
+            States.Add(EnemyFsmStateId.Patrol,  patrolState);
+            States.Add(EnemyFsmStateId.Combat,  combatState);
+            States.Add(EnemyFsmStateId.Alert,  alertState);
+            States.Add(EnemyFsmStateId.Reposition,  repositionState);
+            
+            CurrentState = EnemyFsmStateId.Idle;
+            States[CurrentState].Enter(FsmContext);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Humanoids/RangedAborigine/RangedAborigineFsmFactory.cs
@@ -0,0 +1,17 @@
+﻿using Zenject;
+
+namespace Enemy.Humanoids.RangedAborigine
+{
+    public class RangedAborigineFsmFactory : PlaceholderFactory<HumanoidAiContext, EnemyFsm>
+    {
+        public override EnemyFsm Create(HumanoidAiContext aiContext)
+        {
+            return new RangedAborigineFsm(
+                null,
+                null,
+                null,
+                null,
+                null);
+        }
+    }
+}
