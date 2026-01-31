31e64f9701bb92c8a12a28cf1e8c9e5788f153e3
Hol1k <danilugryumov96@gmail.com>
2026-01-31 09:24:31 +0500

    feat(enemy): add RangedAborigineAiComponent and DI installer for inject Ranged Aborigine factory

---
+++ Assets/Scripts/DiInstallers/Enemies/RangedAborigineAiInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Enemy;
+using Enemy.Humanoids;
+using Enemy.Humanoids.RangedAborigine;
+
+namespace DiInstallers.Enemies
+{
+    public class RangedAborigineAiInstaller : EnemyAiInstaller
+    {
+        protected override void InstallAi()
+        {
+            Container.BindInterfacesAndSelfTo<HumanoidAiContext>().AsSingle();
+            
+            Container.BindFactory<HumanoidAiContext, EnemyFsm, RangedAborigineFsmFactory>().AsSingle();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Humanoids/RangedAborigine/RangedAborigineAiComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Enemy.Humanoids.RangedAborigine
+{
+    public class RangedAborigineAiComponent : MonoBehaviour
+    {
+        private EnemyFsm _fsm;
+
+        [Inject]
+        private void Init(RangedAborigineFsmFactory fsmFactory, HumanoidAiContext aiContext)
+        {
+            _fsm = fsmFactory.Create(aiContext);
+        }
+
+        private void Update()
+        {
+            _fsm.Update();
+        }
+    }
+}
