45d677c4001669fc11aa0c5743e00ae69609759f
Hol1k <danilugryumov96@gmail.com>
2026-02-04 09:46:00 +0500

    feat(rangedAborigine): add RangedAborigineAiContext : HumanoidAiContext for override CanAttackTarget property and CanAttackTargetFrom() method

--- Assets/Scripts/Enemy/Ai/Humanoids/HumanoidAiContext.cs
+++ Assets/Scripts/Enemy/Ai/Humanoids/HumanoidAiContext.cs
@@ -14,47 +14,46 @@ namespace Enemy.Ai.Humanoids
             EnemyCombatComponent combatComponent,
             EnemyShelterRepositionComponent shelterRepositionComponent)
         {
-            _enemyTransform = enemyTransform;
-            _patrolComponent = patrolComponent;
-            _moveController = moveController;
-            _lookComponent = lookComponent;
-            _combatComponent = combatComponent;
-            _shelterRepositionComponent = shelterRepositionComponent;
+            EnemyTransform = enemyTransform;
+            PatrolComponent = patrolComponent;
+            MoveController = moveController;
+            LookComponent = lookComponent;
+            CombatComponent = combatComponent;
+            ShelterRepositionComponent = shelterRepositionComponent;
         }
         
-        public Vector3 EnemyPosition => _enemyTransform.position;
-        private readonly Transform _enemyTransform;
+        public virtual Vector3 EnemyPosition => EnemyTransform.position;
+        protected readonly Transform EnemyTransform;
         
-        public bool IsSeeTarget => _lookComponent.IsSeeTarget;
+        public virtual bool IsSeeTarget => LookComponent.IsSeeTarget;
 
-        public bool IsSeeTargetFrom(Vector3 position) =>
-            _lookComponent.IsSeeTargetFrom(position);
-        public Vector3? LastSeePosition => _lookComponent.LastSeePosition;
-        public float LookRange => _lookComponent.LookRange;
-        public void ForgetLastSeePosition() => _lookComponent.ForgetLastSeePosition();
-        public CharacterStatsComponent ClosestTarget => _lookComponent.GetClosestTarget();
-        private readonly EnemyLookComponent _lookComponent;
+        public virtual bool IsSeeTargetFrom(Vector3 position) =>
+            LookComponent.IsSeeTargetFrom(position);
+        public virtual Vector3? LastSeePosition => LookComponent.LastSeePosition;
+        public virtual float LookRange => LookComponent.LookRange;
+        public virtual void ForgetLastSeePosition() => LookComponent.ForgetLastSeePosition();
+        public virtual CharacterStatsComponent ClosestTarget => LookComponent.GetClosestTarget();
+        protected readonly EnemyLookComponent LookComponent;
 
-        public Vector3 NextPatrolPoint => _patrolComponent.GetNextPoint();
-        private readonly EnemyPatrolComponent _patrolComponent;
+        public virtual Vector3 NextPatrolPoint => PatrolComponent.GetNextPoint();
+        protected readonly EnemyPatrolComponent PatrolComponent;
 
-        public void MoveTo(Vector3 position) => _moveController.MoveTo(position);
-        public void StopMove() => _moveController.StopMove();
-        public void LookAt(Vector3 target) => _moveController.LookAt(target);
-        public bool IsAgentArrivedToDestination => _moveController.IsAgentArrivedToDestination();
-        private readonly EnemyMoveController _moveController;
+        public virtual void MoveTo(Vector3 position) => MoveController.MoveTo(position);
+        public virtual void StopMove() => MoveController.StopMove();
+        public virtual void LookAt(Vector3 target) => MoveController.LookAt(target);
+        public virtual bool IsAgentArrivedToDestination => MoveController.IsAgentArrivedToDestination();
+        protected readonly EnemyMoveController MoveController;
 
-        public bool CanAttackTarget => _combatComponent.CanAttackTarget;
+        public virtual bool CanAttackTarget => CombatComponent.CanAttackTarget;
+        public virtual bool CanAttackTargetFrom(Vector3 position) =>
+            CombatComponent.CanAttackTargetFrom(position);
+        public virtual bool IsTargetEliminated => CombatComponent.IsTargetEliminated;
+        public virtual void AttackTarget(CharacterStatsComponent target) => CombatComponent.AttackTarget(target);
+        protected readonly EnemyCombatComponent CombatComponent;
 
-        public bool CanAttackTargetFrom(Vector3 position) =>
-            _combatComponent.CanAttackTargetFrom(position);
-        public bool IsTargetEliminated => _combatComponent.IsTargetEliminated;
-        public void AttackTarget(CharacterStatsComponent target) => _combatComponent.AttackTarget(target);
-        private readonly EnemyCombatComponent _combatComponent;
-
-        public bool IsOnShelter => _shelterRepositionComponent.IsOnShelter(this);
-        public bool IsShelterPossible => _shelterRepositionComponent.IsShelterPossible(this);
-        public Vector3? FarthestValidShelterPoint => _shelterRepositionComponent.GetFarthestValidShelter(this);
-        private readonly EnemyShelterRepositionComponent _shelterRepositionComponent;
+        public virtual bool IsOnShelter => ShelterRepositionComponent.IsOnShelter(this);
+        public virtual bool IsShelterPossible => ShelterRepositionComponent.IsShelterPossible(this);
+        public Vector3? FarthestValidShelterPoint => ShelterRepositionComponent.GetFarthestValidShelter(this);
+        protected readonly EnemyShelterRepositionComponent ShelterRepositionComponent;
     }
 }
---
+++ Assets/Scripts/Enemy/Ai/Humanoids/RangedAborigine/RangedAborigineAiContext.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Enemy.Ai.Humanoids.RangedAborigine
+{
+    public class RangedAborigineAiContext : HumanoidAiContext
+    {
+        public RangedAborigineAiContext(
+            Transform enemyTransform, 
+            EnemyPatrolComponent patrolComponent, 
+            EnemyMoveController moveController, 
+            EnemyLookComponent lookComponent, 
+            EnemyCombatComponent combatComponent, 
+            EnemyShelterRepositionComponent shelterRepositionComponent) 
+            : base(enemyTransform, patrolComponent, moveController, lookComponent, combatComponent, shelterRepositionComponent)
+        {
+        }
+
+        public override bool CanAttackTarget => throw new System.NotImplementedException();
+        public override bool CanAttackTargetFrom(Vector3 position) => throw new System.NotImplementedException();
+    }
+}
