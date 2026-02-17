2c83bbc898a3f411133c836986b80798caf247a0
Hol1k <danilugryumov96@gmail.com>
2026-02-17 09:11:46 +0500

    feat(enemy): add idle state for vienDevourer

--- Assets/Scripts/Enemy/Ai/VeinDevourer/VeinDevourerAiContext.cs
+++ Assets/Scripts/Enemy/Ai/VeinDevourer/VeinDevourerAiContext.cs
@@ -1,6 +1,18 @@
-﻿namespace Enemy.Ai.VeinDevourer
+﻿using Enemy.Ai.AiContextInterfaces;
+using Entities;
+using UnityEngine;
+
+namespace Enemy.Ai.VeinDevourer
 {
-    public class VeinDevourerAiContext : EnemyAiContext
+    public class VeinDevourerAiContext : EnemyAiContext, IAiLookAgent
     {
+        public bool IsSeeTarget => _lookComponent.IsSeeTarget;
+        public bool IsSeeTargetFrom(Vector3 position) => _lookComponent.IsSeeTargetFrom(position);
+        public Vector3? LastSeePosition => _lookComponent.LastSeePosition;
+        public float LookRange => _lookComponent.LookRange;
+        public void ForgetLastSeePosition() => _lookComponent.ForgetLastSeePosition();
+        public StatsComponent ClosestTarget => _lookComponent.GetClosestTarget();
+        public float ClosestTargetInventoryValue => _lookComponent.ClosestTargetInventoryValue;
+        private readonly EnemyLookComponent _lookComponent;
     }
 }
--- Assets/Scripts/Enemy/Ai/VeinDevourer/VeinDevourerIdleState.cs
+++ Assets/Scripts/Enemy/Ai/VeinDevourer/VeinDevourerIdleState.cs
@@ -1,27 +1,39 @@
-﻿using System;
+﻿using Enemy.Ai.AiContextInterfaces;
+using UnityEngine;
 
 namespace Enemy.Ai.VeinDevourer
 {
     public class VeinDevourerIdleState : EnemyFsmState
     {
+        private readonly IAiLookAgent _lookAgent;
+        
         public VeinDevourerIdleState(VeinDevourerAiContext aiContext)
         {
-            
+            _lookAgent = aiContext;
         }
         
         public override void Enter(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
         }
 
         public override void Update(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            if (fsmContext.IdleTimer > 0f)
+            {
+                fsmContext.IdleTimer -= Time.deltaTime;
+            }
+            else if (_lookAgent.IsSeeTarget && _lookAgent.ClosestTargetInventoryValue > 0.2f)
+            {
+                fsmContext.RequestedState = EnemyFsmStateId.Reposition;
+            }
+            else
+            {
+                fsmContext.IdleTimer = 5f;
+            }
         }
 
         public override void Exit(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
         }
     }
 }
