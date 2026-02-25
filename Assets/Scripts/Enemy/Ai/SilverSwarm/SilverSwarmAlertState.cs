9b32b90d9e3f37461d541c2bb2083fd91c835105
Hol1k <danilugryumov96@gmail.com>
2026-02-25 08:48:06 +0500

    feat(enemy): implement silver swarm alert state

--- Assets/Scripts/Enemy/Ai/SilverSwarm/SilverSwarmAlertState.cs
+++ Assets/Scripts/Enemy/Ai/SilverSwarm/SilverSwarmAlertState.cs
@@ -1,26 +1,56 @@
-﻿using System;
+﻿using Enemy.Ai.AiContextInterfaces;
+using UnityEngine;
 
 namespace Enemy.Ai.SilverSwarm
 {
     public class SilverSwarmAlertState : EnemyFsmState
     {
+        private readonly IAiLookAgent _lookAgent;
+        private readonly IAiMoveAgent _moveAgent;
+        
         public SilverSwarmAlertState(SilverSwarmAiContext aiContext)
         {
+            _lookAgent = aiContext;
+            _moveAgent = aiContext;
         }
 
         public override void Enter(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            fsmContext.LookingTimer = 5f;
         }
 
         public override void Update(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            if (_lookAgent.IsSeeTarget)
+            {
+                fsmContext.RequestedState = EnemyFsmStateId.Combat;
+            }
+            else if (_lookAgent.LastSeePosition == null)
+            {
+                fsmContext.RequestedState = EnemyFsmStateId.Patrol;
+            }
+            else
+            {
+                _moveAgent.MoveTo(_lookAgent.LastSeePosition.Value);
+                if (!_moveAgent.IsAgentArrivedToDestination)
+                {
+                    _moveAgent.MoveTo(_lookAgent.LastSeePosition.Value);
+                }
+                else if (fsmContext.LookingTimer > 0f)
+                {
+                    fsmContext.LookingTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    fsmContext.RequestedState = EnemyFsmStateId.Patrol;
+                }
+            }
         }
 
         public override void Exit(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            _moveAgent.StopMove();
+            fsmContext.LookingTimer = 0f;
         }
     }
 }
