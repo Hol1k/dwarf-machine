46da97c02eeda91504e6ea674f0bf9c70ec3df5f
Hol1k <danilugryumov96@gmail.com>
2026-02-25 08:56:01 +0500

    feat(enemy): implement silver swarm reposition state

--- Assets/Scripts/Enemy/Ai/SilverSwarm/SilverSwarmRepositionState.cs
+++ Assets/Scripts/Enemy/Ai/SilverSwarm/SilverSwarmRepositionState.cs
@@ -1,26 +1,53 @@
 ﻿using System;
+using Enemy.Ai.AiContextInterfaces;
 
 namespace Enemy.Ai.SilverSwarm
 {
     public class SilverSwarmRepositionState : EnemyFsmState
     {
+        private readonly IAiCombatAgent _combatAgent;
+        private readonly IAiShelterRepositionAgent _repositionAgent;
+        private readonly IAiMoveAgent _moveAgent;
+        
         public SilverSwarmRepositionState(SilverSwarmAiContext aiContext)
         {
+            _combatAgent = aiContext;
+            _repositionAgent = aiContext;
+            _moveAgent = aiContext;
         }
 
         public override void Enter(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
         }
 
         public override void Update(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            if (_combatAgent.IsTargetEliminated)
+            {
+                fsmContext.RequestedState = EnemyFsmStateId.Patrol;
+            }
+            else if (!fsmContext.RepositionPoint.HasValue ||
+                     !_combatAgent.CanAttackTargetFrom(fsmContext.RepositionPoint.Value))
+            {
+                fsmContext.RepositionPoint = _repositionAgent.FarthestValidShelterPoint;
+            }
+            else
+            {
+                _moveAgent.MoveTo(fsmContext.RepositionPoint.Value);
+                if (!_moveAgent.IsAgentArrivedToDestination)
+                {
+                    _moveAgent.MoveTo(fsmContext.RepositionPoint.Value);
+                }
+                else
+                {
+                    fsmContext.RequestedState = EnemyFsmStateId.Combat;
+                }
+            }
         }
 
         public override void Exit(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            _moveAgent.StopMove();
         }
     }
 }
