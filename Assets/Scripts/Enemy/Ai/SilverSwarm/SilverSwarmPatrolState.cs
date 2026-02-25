ca2cb4ab80115f15f599ffed6ddbf376f41ee4f0
Hol1k <danilugryumov96@gmail.com>
2026-02-25 08:26:31 +0500

    feat(enemy): implement silver swarm patrol state

--- Assets/Scripts/Enemy/Ai/SilverSwarm/SilverSwarmPatrolState.cs
+++ Assets/Scripts/Enemy/Ai/SilverSwarm/SilverSwarmPatrolState.cs
@@ -1,26 +1,53 @@
-﻿using System;
+﻿using Enemy.Ai.AiContextInterfaces;
 
 namespace Enemy.Ai.SilverSwarm
 {
     public class SilverSwarmPatrolState : EnemyFsmState
     {
+        private readonly IAiLookAgent _lookAgent;
+        private readonly IAiMoveAgent _moveAgent;
+        private readonly IAiPatrolAgent _patrolAgent;
+        
         public SilverSwarmPatrolState(SilverSwarmAiContext aiContext)
         {
+            _lookAgent = aiContext;
+            _moveAgent = aiContext;
+            _patrolAgent = aiContext;
         }
 
         public override void Enter(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            _lookAgent.ForgetLastSeePosition();
         }
 
         public override void Update(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            if (_lookAgent.IsSeeTarget)
+            {
+                fsmContext.RequestedState = EnemyFsmStateId.Combat;
+            }
+            else if (!fsmContext.RepositionPoint.HasValue)
+            {
+                fsmContext.RepositionPoint = _patrolAgent.NextPatrolPoint;
+            }
+            else
+            {
+                _moveAgent.MoveTo(fsmContext.RepositionPoint.Value);
+                if (!_moveAgent.IsAgentArrivedToDestination)
+                {
+                    _moveAgent.MoveTo(fsmContext.RepositionPoint.Value);
+                }
+                else
+                {
+                    fsmContext.PatrolPoint = null;
+                    fsmContext.RequestedState = EnemyFsmStateId.Idle;
+                }
+            }
         }
 
         public override void Exit(EnemyFsmContext fsmContext)
         {
-            throw new NotImplementedException();
+            _moveAgent.StopMove();
         }
     }
 }
