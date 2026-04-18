337b391b3422ed5d60c99bc71c2d160ddb85f744
Hol1k <danilugryumov96@gmail.com>
2026-04-18 10:16:14 +0500

    chore(scene): add aborigine's room to test scene

---
+++ Assets/Scripts/EditorScripts/GizmosPointDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace EditorScripts
+{
+    public class GizmosPointDrawer : MonoBehaviour
+    {
+        [SerializeField] private Color color;
+        [SerializeField] [Min(0)] private float radius;
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = color;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+}
--- Assets/Scripts/PointsOfInterest/PointOfInterest.cs
+++ Assets/Scripts/PointsOfInterest/PointOfInterest.cs
@@ -13,10 +13,6 @@ namespace PointsOfInterest
         [SerializeField] private Transform patrolPointsCollectionParent;
         [SerializeField] private Transform shelterRepositionPointsCollectionParent;
         [SerializeField] private Transform spawnPointsCollectionParent;
-        
-        [SerializeField] private Color patrolPointColor;
-        [SerializeField] private Color repositionPointColor;
-        [SerializeField] private Color spawnPointColor;
 
         public EnemyPatrolPointsCollection PatrolPointsCollection => new(
             patrolPointsCollectionParent
@@ -33,34 +29,6 @@ namespace PointsOfInterest
             spawnPointsCollectionParent
                 .GetComponentsInChildren<Transform>()
                 .Where(point => point != spawnPointsCollectionParent).ToArray());
-
-        private void OnDrawGizmosSelected()
-        {
-            var patrolPoints = patrolPointsCollectionParent.GetComponentsInChildren<Transform>()
-                .Where(point => point != patrolPointsCollectionParent).ToArray();
-            var repositionPoints = shelterRepositionPointsCollectionParent.GetComponentsInChildren<Transform>()
-                .Where(point => point != shelterRepositionPointsCollectionParent).ToArray();
-            var spawnPoints = spawnPointsCollectionParent.GetComponentsInChildren<Transform>()
-                .Where(point => point != shelterRepositionPointsCollectionParent).ToArray();
-
-            foreach (var point in patrolPoints)
-            {
-                Gizmos.color = patrolPointColor;
-                Gizmos.DrawSphere(point.position, 0.3f);
-            }
-
-            foreach (var point in repositionPoints)
-            {
-                Gizmos.color = repositionPointColor;
-                Gizmos.DrawSphere(point.position, 0.3f);
-            }
-
-            foreach (var point in spawnPoints)
-            {
-                Gizmos.color = spawnPointColor;
-                Gizmos.DrawSphere(point.position, 0.3f);
-            }
-        }
     }
 
     public enum PoiType
