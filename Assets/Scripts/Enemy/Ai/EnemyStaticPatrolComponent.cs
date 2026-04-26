5611354f2baf3fd33d747f264c4fd1a0201c5efc
Hol1k <danilugryumov96@gmail.com>
2026-04-26 12:31:03 +0500

    feat(enemy): enemies start patrol from random point of collection

--- Assets/Scripts/Enemy/Ai/EnemyStaticPatrolComponent.cs
+++ Assets/Scripts/Enemy/Ai/EnemyStaticPatrolComponent.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Enemy.Ai
 {
@@ -8,6 +9,11 @@ namespace Enemy.Ai
         [Inject] private EnemyPatrolPointsCollection patrolPoints;
         
         private int _currentPatrolPointIndex = int.MaxValue;
+
+        private void Awake()
+        {
+            _currentPatrolPointIndex = Random.Range(0, patrolPoints.Length);
+        }
 
         public override Vector3 GetNextPoint(Vector3 startPos)
         {
