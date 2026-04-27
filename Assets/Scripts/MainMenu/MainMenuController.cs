e10892d14249d8d38cfe5c254d66d64dd32487f4
Hol1k <danilugryumov96@gmail.com>
2026-04-27 17:44:18 +0500

    feat(scens): add test level loading script for game menu

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace MainMenu
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        private const string GameplayScenePath = "Scenes/Gameplay";
+        private const string TestLevelScenePath = "Scenes/TestLevel";
+        
+        public void OnStartButton()
+        {
+            StartTestLevel().Forget();
+        }
+
+        private async UniTask StartTestLevel()
+        {
+            DontDestroyOnLoad(this);
+
+            await Addressables.LoadSceneAsync(GameplayScenePath);
+            await Addressables.LoadSceneAsync(TestLevelScenePath, LoadSceneMode.Additive);
+            
+            Destroy(gameObject);
+        }
+    }
+}
