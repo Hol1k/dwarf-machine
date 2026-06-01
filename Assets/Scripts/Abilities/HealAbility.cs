2e2f39dd4f1a557b7434bb0ac5bcb0095c2e5d75
Hol1k <danilugryumov96@gmail.com>
2026-06-01 11:57:01 +0500

    feat(abilities): add heal ability

---
+++ Assets/Scripts/Abilities/HealAbility.cs
@@ -0,0 +1,21 @@
+﻿using Character;
+using Entities;
+using UnityEngine;
+
+namespace Abilities
+{
+    [CreateAssetMenu(fileName = "NewHealAbility", menuName = "Abilities/HealAbility", order = 0)]
+    public class HealAbility : Ability
+    {
+        public int healAmount;
+        [SerializeField] [Min(0)] private float cooldown;
+        public override float Cooldown => cooldown;
+
+        public override void Cast(CharacterAbilitiesController handler)
+        {
+            if (handler.TryGetComponent(out IDamageable damageable))
+                damageable.Health += healAmount;
+            Debug.Log("heal cast");
+        }
+    }
+}
