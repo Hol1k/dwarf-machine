e03d2a611a20a0de169a7b854df3182d67cf0fed
Hol1k <danilugryumov96@gmail.com>
2026-06-01 14:48:23 +0500

    feat(abilities): add drone ability

---
+++ Assets/Scripts/Abilities/GunDroneAbility.cs
@@ -0,0 +1,37 @@
+﻿using Character;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Abilities
+{
+    [CreateAssetMenu(fileName = "NewGunDroneAbility", menuName = "Abilities/GunDroneAbility", order = 0)]
+    public class GunDroneAbility : Ability
+    {
+        [SerializeField] AssetReference dronePrefab;
+
+        [SerializeField] private float droneLifeTime;
+        [SerializeField] [Min(0)] private float cooldown;
+        public override float Cooldown => cooldown;
+        
+        [Space]
+        [SerializeField] private float attackSpeed;
+        [SerializeField] private float damage;
+
+        public override void Cast(CharacterAbilitiesController handler)
+        {
+            CastDroneAsync(handler).Forget();
+        }
+
+        private async UniTask CastDroneAsync(CharacterAbilitiesController handler)
+        {
+            var drone = await dronePrefab.InstantiateAsync(handler.transform.position, new Quaternion()).ToUniTask();
+            var droneComponent = drone.GetComponent<GunDroneComponent>();
+            droneComponent.droneHandler = handler.transform;
+            droneComponent.attackSpeed = attackSpeed;
+            droneComponent.damage = damage;
+            await UniTask.WaitForSeconds(droneLifeTime);
+            Addressables.Release(drone);
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/GunDroneComponent.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using Entities;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Abilities
+{
+    public class GunDroneComponent : MonoBehaviour
+    {
+        public float attackSpeed;
+        public float damage;
+        [SerializeField] private LayerMask targetMask;
+
+        public Transform droneHandler;
+        [SerializeField] Vector3 positionOffset;
+
+        private readonly List<GameObject> _visibleTargets = new();
+
+        private float _lastAttackTime = 0;
+
+        private void Update()
+        {
+            var closestTarget = CalculateClosestTarget();
+
+            FollowToHandler();
+            
+            if (!closestTarget)
+            {
+                transform.rotation = droneHandler.rotation;
+                return;
+            }
+            transform.LookAt(closestTarget.transform);
+            if (Time.time - _lastAttackTime > 60 / attackSpeed &&
+                closestTarget.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(damage);
+                _lastAttackTime = Time.time;
+            }
+        }
+
+        private void FollowToHandler()
+        {
+            transform.position = Vector3.Lerp(transform.position, droneHandler.position + droneHandler.rotation * positionOffset, 0.2f);
+        }
+
+        private GameObject CalculateClosestTarget()
+        {
+            var closestDistance = float.MaxValue;
+            GameObject closestTarget = null;
+            for (int i = _visibleTargets.Count - 1; i >= 0; i--)
+            {
+                if (_visibleTargets[i].IsDestroyed())
+                {
+                    _visibleTargets.Remove(_visibleTargets[i]);
+                    continue;
+                }
+
+                var currDistance = Vector3.Distance(transform.position, _visibleTargets[i].transform.position);
+                if (!closestTarget ||
+                    currDistance < closestDistance)
+                {
+                    closestTarget = _visibleTargets[i];
+                    closestDistance = currDistance;
+                }
+            }
+            
+            return closestTarget;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (1 << other.gameObject.layer == targetMask.value)
+            {
+                _visibleTargets.Add(other.gameObject);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            _visibleTargets.Remove(other.gameObject);
+        }
+    }
+}
