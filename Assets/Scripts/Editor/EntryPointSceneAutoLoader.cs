2883824a199b915757d61b75a4d60da93cc550fa
Hol1k <danilugryumov96@gmail.com>
2026-06-03 12:40:17 +0500

    feat(editor): add EntryPointSceneAutoLoader scripts

---
+++ Assets/Scripts/Editor/EntryPointSceneAutoLoader.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Project.Editor
+{
+    [InitializeOnLoad]
+    public static class EntryPointSceneAutoLoader
+    {
+        private const string MenuPath = "PlayFromBootstrap/Enabled";
+        private const string PlatFromBootstrapKey = "PlatFromBootstrapKey";
+        private const int BootSceneIndex = 0;
+        
+        static EntryPointSceneAutoLoader()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        [MenuItem(MenuPath)]
+        private static void Toggle()
+        {
+            bool result = EditorPrefs.GetBool(PlatFromBootstrapKey);
+            EditorPrefs.SetBool(PlatFromBootstrapKey, !result);
+        }
+
+        [MenuItem(MenuPath, true)]
+        private static bool ToggleValidate()
+        {
+            Menu.SetChecked(MenuPath, EditorPrefs.GetBool(PlatFromBootstrapKey));
+            return true;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode)
+            {
+                if (EditorPrefs.GetBool(PlatFromBootstrapKey) == false)
+                {
+                    EditorSceneManager.playModeStartScene = null;
+                    return;
+                }
+                
+                if (EditorBuildSettings.scenes.Length == 0)
+                    return;
+                
+                EditorSceneManager.playModeStartScene = AssetDatabase
+                    .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[BootSceneIndex].path);
+            }
+        }
+    }
+}
